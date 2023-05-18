ffaad4cd93e66c1d8b2c9102d06c5de5f1bedbf7
Sorawit <6330528521@student.chula.ac.th>
2023-05-18 16:26:51 +0700

    fix code in model.py

--- model.py
+++ model.py
@@ -1,4 +1,44 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn import metrics
 
-model = GridSearchCV(estimator=RandomForestRegressor(),param_grid=dict(criterion=['gini','entropy'],max_depth=[2,3,6],min_samples_leaf=[2,5,10],
-    n_estimators=[100,200], random_state=[2020]),scoring='f1_weighted',cv=5,n_jobs=-1
-) 
+path = "./data_cleaned.csv"
+data = pd.read_csv(path)
+
+nominal_columns = ["type","district"]
+
+dummy_df = pd.get_dummies(data[nominal_columns], drop_first=False) 
+data_df = pd.concat([data, dummy_df], axis=1)
+data_df = data_df.drop(nominal_columns, axis=1)
+
+y = data_df.pop('duration_hour')
+X = data_df.drop("timestamp",axis = 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2020)
+
+param_grid = {
+    'bootstrap': [True,False],
+    'max_depth': [40,60,80],
+    'max_features': [10,20],
+    'min_samples_leaf': [3,4,5],
+    'min_samples_split': [12,24,36],
+    'n_estimators': [120,200,400]
+}
+model = GridSearchCV(estimator=RandomForestRegressor(),param_grid= param_grid,cv=3,n_jobs=-1,verbose= 2) 
+
+model_RF = RandomForestRegressor(bootstrap = False, max_depth = 40, max_features = 10, min_samples_leaf = 3, min_samples_split = 36, n_estimators = 120)
+
+model_RF.fit(X_train, y_train)
+
+predictions = model_RF.predict(X_test)
+
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
+
+
+
+
